bf1401ba5e748efbc725983aa9940d9a6dbd14d6
Nick-Dillon <ndillon18@hotmail.co.uk>
2019-04-15 12:28:43 +0100

    add a simple router for GET requests

--- main.go
+++ main.go
@@ -1,7 +1,34 @@
+/*
+1. need a server
+	- gorilla/mux (go's version of express.js) - Gorilla is a CRUD
+		DONE
+2. receive a basic request
+	- make a simple get request, using insomnia, to check code works
+		DONE
+	-
+
+*/
+
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func returnHello(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode("Hello there!")
+}
 
 func main() {
-	fmt.Println("This is the backend")
+	router := mux.NewRouter()
+	router.HandleFunc("/api/yturl", returnHello).Methods("GET")
+	// router.HandleFunc("/api/yturl", getAudio).Methods("POST")
+	// router.HandleFunc("/api/search-terms", getScriptandTimestamps).Methods("POST")
+	// http.Handle("/", router)
+
+	log.Fatal(http.ListenAndServe(":8000", router))
 }

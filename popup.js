cd15eda66fa222f3b2f75c4e58f51ac70b858994
Challen Bellamey <challenbellamey@hotmail.com>
2019-04-16 15:38:55 +0100

    Add XMLHttpRequest for local testing

--- popup.js
+++ popup.js
@@ -1,18 +1,27 @@
-import {postUrl, postKeyWords} from "./axios";
+const backendUrl = 'http://localhost:8000/api/yturl/';
 
 function getUrl() {
   chrome.tabs.query({ active: true }, function(tabs) {
     console.log(tabs);
     const url = tabs[0].url;
-    console.log(url);
-    postUrl(url).then(res => console.log(res));
+    const ytparams = url
+      .split('?')[1]
+      .split('&')
+      .reduce((acc, val) => {
+        let [k, v] = val.split('=');
+        acc[k] = v;
+        return acc;
+      }, {});
+    console.log(ytparams);
+    const req = new XMLHttpRequest();
+    req.open("GET", `${backendUrl}${ytparams.v}`, false);
+    req.send();
   });
 };
 
 function submitKeyWords() {
   let keyword = document.getElementById("keyword").value;
   console.log(keyword);
-  postKeyWords(keyword).then(console.log);
 };
 
 document

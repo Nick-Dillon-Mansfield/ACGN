dc8219ef4874791aebba291df92daf95c52f7a35
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-19 14:10:30 +0100

    display / hide script

--- popup.js
+++ popup.js
@@ -30,26 +30,30 @@ document.getElementById("buttonUrl").addEventListener("click", function() {
 document
   .getElementById("submitButtonKeyWord")
   .addEventListener("click", function() {
-    const listCount = document.getElementById("listCount")
-    const list = document.getElementById("list")
+    const listCount = document.getElementById("listCount");
+    const list = document.getElementById("list");
     console.log(list.childNodes.length);
     if (list.childNodes.length > 1) {
-      console.log("SIMON DETECTS LIST!")
+      console.log("SIMON DETECTS LIST!");
       console.log(list);
-      list.removeChild(list.lastElementChild)
+      list.removeChild(list.lastElementChild);
       console.log(list);
     }
-    const keyword = document.getElementById("keyword").value
-    const matchingWords = filterKeyword(fakeScript, keyword)
+    const keyword = document.getElementById("keyword").value;
+    const matchingWords = filterKeyword(fakeScript, keyword);
     if (matchingWords.length === 0) {
-      listCount.innerText = `I cannot find "${keyword}" in the video, sorry :O`
+      listCount.innerText = `I cannot find "${keyword}" in the video, sorry :O`;
     } else {
-      listCount.innerText = `I have found "${keyword}" ${matchingWords.length} time(s)`;
-      const listArea = document.getElementById("list")
-      let newList = document.createElement('ol');
+      listCount.innerText = `I have found "${keyword}" ${
+        matchingWords.length
+      } time(s)`;
+      const listArea = document.getElementById("list");
+      let newList = document.createElement("ol");
       for (let i = 0; i < matchingWords.length; i++) {
-        let listItem = document.createElement('li')
-        listItem.appendChild(document.createTextNode(matchingWords[i].end_time));
+        let listItem = document.createElement("li");
+        listItem.appendChild(
+          document.createTextNode(matchingWords[i].end_time)
+        );
         newList.appendChild(listItem);
       }
       listArea.appendChild(newList);
@@ -67,11 +71,22 @@ const filterKeyword = (script, keyword) => {
   return filtered;
 };
 
-document
-  .getElementById("testLink")
-  .addEventListener("click", function() {
-    chrome.tabs.update(youtubeURL[0], {url: youtubeURL[1]+"&t=60"})
-  })
+const scriptButton = document.getElementById("scriptButton");
+const scriptArea = document.getElementById("scriptArea");
+scriptButton.addEventListener("click", function() {
+  scriptButton.innerText === "Show Script"
+    ? (scriptButton.innerText = "Hide Script")
+    : scriptButton.innerText === "Hide Script"
+    ? (scriptButton.innerText = "Show Script")
+    : null;
+  if (scriptButton.innerText === "Hide Script") {
+    let fullScript = document.createElement("p");
+    fullScript.appendChild(document.createTextNode(fakeScript.transcript));
+    scriptArea.appendChild(fullScript);
+  } else {
+    scriptArea.removeChild(scriptArea.lastElementChild);
+  }
+});
 
 /*
 1. keyword must be known
@@ -81,37 +96,38 @@ document
 */
 
 const fakeScript = {
-  "transcript": "Hello this will be our test script It repeats some of our words like script and test and script and test",
-  "confidence": "0.99",
-  "words": [
-    { "start_time": "0", "end_time": "1", "word": "Hello" },
-    { "start_time": "1", "end_time": "2", "word": "this" },
-    { "start_time": "2", "end_time": "3", "word": "will" },
-    { "start_time": "3", "end_time": "4", "word": "be" },
-    { "start_time": "4", "end_time": "5", "word": "our" },
-    { "start_time": "5", "end_time": "6", "word": "test" },
-    { "start_time": "6", "end_time": "7", "word": "script" },
-    { "start_time": "7", "end_time": "8", "word": "It" },
-    { "start_time": "8", "end_time": "9", "word": "repeats" },
-    { "start_time": "9", "end_time": "10", "word": "some" },
-    { "start_time": "10", "end_time": "11", "word": "of" },
-    { "start_time": "11", "end_time": "12", "word": "our" },
-    { "start_time": "12", "end_time": "13", "word": "words" },
-    { "start_time": "13", "end_time": "14", "word": "like" },
+  transcript:
+    "Hello this will be our test script It repeats some of our words like script and test and script and test",
+  confidence: "0.99",
+  words: [
+    { start_time: "0", end_time: "1", word: "Hello" },
+    { start_time: "1", end_time: "2", word: "this" },
+    { start_time: "2", end_time: "3", word: "will" },
+    { start_time: "3", end_time: "4", word: "be" },
+    { start_time: "4", end_time: "5", word: "our" },
+    { start_time: "5", end_time: "6", word: "test" },
+    { start_time: "6", end_time: "7", word: "script" },
+    { start_time: "7", end_time: "8", word: "It" },
+    { start_time: "8", end_time: "9", word: "repeats" },
+    { start_time: "9", end_time: "10", word: "some" },
+    { start_time: "10", end_time: "11", word: "of" },
+    { start_time: "11", end_time: "12", word: "our" },
+    { start_time: "12", end_time: "13", word: "words" },
+    { start_time: "13", end_time: "14", word: "like" },
     {
-      "start_time": "14",
-      "end_time": "15",
-      "word": "script"
+      start_time: "14",
+      end_time: "15",
+      word: "script"
     },
-    { "start_time": "15", "end_time": "16", "word": "and" },
-    { "start_time": "16", "end_time": "17", "word": "test" },
-    { "start_time": "17", "end_time": "18", "word": "and" },
+    { start_time: "15", end_time: "16", word: "and" },
+    { start_time: "16", end_time: "17", word: "test" },
+    { start_time: "17", end_time: "18", word: "and" },
     {
-      "start_time": "18",
-      "end_time": "19",
-      "word": "script"
+      start_time: "18",
+      end_time: "19",
+      word: "script"
     },
-    { "start_time": "19", "end_time": "20", "word": "and" },
-    { "start_time": "20", "end_time": "21", "word": "Test" }
+    { start_time: "19", end_time: "20", word: "and" },
+    { start_time: "20", end_time: "21", word: "Test" }
   ]
- }
+};

6a28e380a6875381ac32ce33c557dc59e82cd767
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-19 16:01:51 +0100

    delete start and end time and replace with time

--- popup.js
+++ popup.js
@@ -4,10 +4,9 @@ const youtubeURL = [];
 
 function getUrl() {
   chrome.tabs.query({ active: true }, function(tabs) {
-    console.log(tabs[0]);
     const url = tabs[0].url;
     youtubeURL.push(tabs[0].id, url);
-    console.log(youtubeURL);
+
     const ytparams = url
       .split("?")[1]
       .split("&")
@@ -16,7 +15,7 @@ function getUrl() {
         acc[k] = v;
         return acc;
       }, {});
-    console.log(ytparams);
+
     const req = new XMLHttpRequest();
     req.open("GET", `${BASE_URL}yturl/${ytparams.v}`, false);
     req.send();
@@ -25,6 +24,9 @@ function getUrl() {
 
 document.getElementById("buttonUrl").addEventListener("click", function() {
   getUrl();
+  const body = document.getElementById("body");
+  const genScriptButtonDiv = document.getElementById("genScriptButtonDiv");
+  body.removeChild(genScriptButtonDiv);
 });
 
 document
@@ -100,34 +102,32 @@ const fakeScript = {
     "Hello this will be our test script It repeats some of our words like script and test and script and test",
   confidence: "0.99",
   words: [
-    { start_time: "0", end_time: "1", word: "Hello" },
-    { start_time: "1", end_time: "2", word: "this" },
-    { start_time: "2", end_time: "3", word: "will" },
-    { start_time: "3", end_time: "4", word: "be" },
-    { start_time: "4", end_time: "5", word: "our" },
-    { start_time: "5", end_time: "6", word: "test" },
-    { start_time: "6", end_time: "7", word: "script" },
-    { start_time: "7", end_time: "8", word: "It" },
-    { start_time: "8", end_time: "9", word: "repeats" },
-    { start_time: "9", end_time: "10", word: "some" },
-    { start_time: "10", end_time: "11", word: "of" },
-    { start_time: "11", end_time: "12", word: "our" },
-    { start_time: "12", end_time: "13", word: "words" },
-    { start_time: "13", end_time: "14", word: "like" },
+    { time: "1", word: "Hello" },
+    { time: "2", word: "this" },
+    { time: "3", word: "will" },
+    { time: "4", word: "be" },
+    { time: "5", word: "our" },
+    { time: "6", word: "test" },
+    { time: "7", word: "script" },
+    { time: "8", word: "It" },
+    { time: "9", word: "repeats" },
+    { time: "10", word: "some" },
+    { time: "11", word: "of" },
+    { time: "12", word: "our" },
+    { time: "13", word: "words" },
+    { time: "14", word: "like" },
     {
-      start_time: "14",
-      end_time: "15",
+      time: "15",
       word: "script"
     },
-    { start_time: "15", end_time: "16", word: "and" },
-    { start_time: "16", end_time: "17", word: "test" },
-    { start_time: "17", end_time: "18", word: "and" },
+    { time: "16", word: "and" },
+    { time: "17", word: "test" },
+    { time: "18", word: "and" },
     {
-      start_time: "18",
-      end_time: "19",
+      time: "19",
       word: "script"
     },
-    { start_time: "19", end_time: "20", word: "and" },
-    { start_time: "20", end_time: "21", word: "Test" }
+    { time: "20", word: "and" },
+    { time: "21", word: "Test" }
   ]
 };

a27f1ec02ea7c1b7a0ce77076c093d8e1d1b96fb
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-22 10:24:32 +0100

    add static data of res object

--- popup.js
+++ popup.js
@@ -96,3 +96,40 @@ scriptButton.addEventListener("click", function() {
     scriptArea.removeChild(scriptArea.lastElementChild);
   }
 });
+
+const fakeScript = {
+  transcript:
+    "Hello this will be our test script It repeats some of our words like script and test and script and test",
+  confidence: "0.99",
+  words: [
+    { time: "1", word: "Hello" },
+    { time: "2", word: "this" },
+    { time: "3", word: "will" },
+    { time: "4", word: "be" },
+    { time: "5", word: "our" },
+    { time: "6", word: "test" },
+    { time: "7", word: "script" },
+    { time: "8", word: "It" },
+    { time: "9", word: "repeats" },
+    { time: "10", word: "some" },
+    { time: "11", word: "of" },
+    { time: "12", word: "our" },
+    { time: "13", word: "words" },
+    { time: "14", word: "like" },
+    {
+      start_time: "14",
+      end_time: "15",
+      word: "script"
+    },
+    { start_time: "15", end_time: "16", word: "and" },
+    { start_time: "16", end_time: "17", word: "test" },
+    { start_time: "17", end_time: "18", word: "and" },
+    {
+      start_time: "18",
+      end_time: "19",
+      word: "script"
+    },
+    { start_time: "19", end_time: "20", word: "and" },
+    { start_time: "20", end_time: "21", word: "Test" }
+  ]
+};

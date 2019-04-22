0013cf87051fb2905458a8b3e045a4da80d84ac2
Georgia Lumley <georgia@Georgias-MacBook-Pro.local>
2019-04-22 10:18:19 +0100

    frontend can get res back from backend

--- popup.js
+++ popup.js
@@ -1,6 +1,9 @@
 const BASE_URL = "http://localhost:8000/api/";
 
 const youtubeURL = [];
+let transcript;
+let confidence;
+let words;
 
 function getUrl() {
   chrome.tabs.query({ active: true }, function(tabs) {
@@ -18,10 +21,12 @@ function getUrl() {
 
     const req = new XMLHttpRequest();
     req.open("GET", `${BASE_URL}yturl/${ytparams.v}`, false);
-    req.send()
-      .then((res) => {
-        console.log(res.body)
-      })
+    req.send();
+    const response = JSON.parse(req.response);
+    transcript = response.transcript;
+    confidence = response.confidence;
+    words = response.words;
+    console.log(transcript);
   });
 }
 
@@ -45,7 +50,8 @@ document
       console.log(list);
     }
     const keyword = document.getElementById("keyword").value;
-    const matchingWords = filterKeyword(fakeScript, keyword);
+    const matchingWords = filterKeyword(keyword);
+    console.log(matchingWords);
     if (matchingWords.length === 0) {
       listCount.innerText = `I cannot find "${keyword}" in the video, sorry :O`;
     } else {
@@ -56,9 +62,7 @@ document
       let newList = document.createElement("ol");
       for (let i = 0; i < matchingWords.length; i++) {
         let listItem = document.createElement("li");
-        listItem.appendChild(
-          document.createTextNode(matchingWords[i].time)
-        );
+        listItem.appendChild(document.createTextNode(matchingWords[i].time));
         newList.appendChild(listItem);
       }
       listArea.appendChild(newList);
@@ -66,10 +70,10 @@ document
     document.getElementById("keyword").value = "";
   });
 
-const filterKeyword = (script, keyword) => {
-  console.log("the keyword is" + keyword);
+const filterKeyword = keyword => {
+  console.log("the keyword is " + keyword);
 
-  const filtered = script.words.filter(
+  const filtered = words.filter(
     word => word.word.toLowerCase() === keyword.toLowerCase()
   );
 
@@ -86,51 +90,9 @@ scriptButton.addEventListener("click", function() {
     : null;
   if (scriptButton.innerText === "Hide Script") {
     let fullScript = document.createElement("p");
-    fullScript.appendChild(document.createTextNode(fakeScript.transcript));
+    fullScript.appendChild(document.createTextNode(transcript));
     scriptArea.appendChild(fullScript);
   } else {
     scriptArea.removeChild(scriptArea.lastElementChild);
   }
 });
-
-/*
-1. keyword must be known
-2. we must have the script
-3. search through the script with the keyword to find all instances of it, with their times
-4. forEach instance, generate new list item, with the item being an <a> tag (to link to the timestamp)
-*/
-
-const fakeScript = {
-  transcript:
-    "Hello this will be our test script It repeats some of our words like script and test and script and test",
-  confidence: "0.99",
-  words: [
-    { time: "1", word: "Hello" },
-    { time: "2", word: "this" },
-    { time: "3", word: "will" },
-    { time: "4", word: "be" },
-    { time: "5", word: "our" },
-    { time: "6", word: "test" },
-    { time: "7", word: "script" },
-    { time: "8", word: "It" },
-    { time: "9", word: "repeats" },
-    { time: "10", word: "some" },
-    { time: "11", word: "of" },
-    { time: "12", word: "our" },
-    { time: "13", word: "words" },
-    { time: "14", word: "like" },
-    {
-      time: "15",
-      word: "script"
-    },
-    { time: "16", word: "and" },
-    { time: "17", word: "test" },
-    { time: "18", word: "and" },
-    {
-      time: "19",
-      word: "script"
-    },
-    { time: "20", word: "and" },
-    { time: "21", word: "Test" }
-  ]
-};
